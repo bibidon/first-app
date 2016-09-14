79bcb9128b9e48e48db5cedc2f1faff803c20a5f
bibidon <debibidon@gmail.com>
2016-09-14 23:34:30 +0300

    multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { OtherComponent, AnotherComponent } from './other'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  template: `
-    Hello World!
-    <app-other></app-other>
-  `
-  //templateUrl: 'app.component.html',
-  //styleUrls: ['app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  directives: [OtherComponent, AnotherComponent]
 })
 export class AppComponent {
   title = 'app works!';
